7cdac1d3efa479ba2832e06ba85f596e7ccdca48
Dricmoy Bhattacharjee <dricbhattacharjee@gmail.com>
2024-10-07 13:47:30 -0600

    partial sum calculation function

--- MISC/TruncationErrorAlternatingSeries.py
+++ MISC/TruncationErrorAlternatingSeries.py
@@ -1,4 +1,8 @@
 import sympy as sp
+import numpy as np
+
+def S_n(n):
+    return 2 * np.sqrt(3) * sum((-1)**k / (3**k * (2*k + 1)) for k in range(n + 1))
 
 # Define the symbol and error expression
 n = sp.symbols('n', integer=True)
@@ -16,5 +20,12 @@ while error_expr.subs(n, n_value) >= tolerance:
 print(f"The smallest n such that the error is less than 10^-3 is: {n_value}")
 
 # Test the value of n by substituting it back into the error expression
-error_at_n = error_expr.subs(n, n_value)
+error_at_n = error_expr.subs(n, 4)
 print(f"Error at n = {n_value}: {error_at_n}")
+
+
+# Compute the partial sum for n=4
+n = 4
+S4 = S_n(n)
+print(f"Partial sum S_4: {S4}")
+
